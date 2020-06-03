4f002a0b73eacae379f61524ae4a56582807e171
MapleRen <kl941104@163.com>
2020-06-03 20:27:06 +0800

    1
    测试

---
+++ gbf_blhxfy.js
@@ -0,0 +1,31 @@
+//https://github.com/Yesterday17/Surge-Script/blob/master/gbf/gbf_blhxfy.js
+
+function insert(str, index, value) {
+    return str.substr(0, index) + value + str.substr(index);
+  }
+  
+  const toInsert = `
+  <script>
+  (function () {
+    const script = document.createElement("script");
+    script.src = "https://blhx.danmu9.com/blhxfy/extension.user.js";
+    document.head.appendChild(script);
+  })();
+  </script>
+  `;
+  
+  let { body, headers } = $response;
+  const result = {};
+  
+  if (
+    body &&
+    headers &&
+    typeof headers["Content-Type"] === "string" &&
+    headers["Content-Type"].includes("text/html") &&
+    body.lastIndexOf("</head>") !== -1
+  ) {
+    result.body = insert(body, body.lastIndexOf("</head>"), toInsert);
+    result.headers = headers;
+  }
+  
+  $done(result);
