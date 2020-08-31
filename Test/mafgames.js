7184360f14026598a5409dd9b7fa54cf18594ebd
MapleRen <kl941104@163.com>
2020-08-31 12:34:31 +0800

    动态请求全文件

--- Test/mafgames.js
+++ Test/mafgames.js
@@ -32,20 +32,27 @@ function modifyData(data,mod,key){
 }
 
 if(isUpperVersion){
-     var version = $prefs.valueForKey('tileRPG_version');
-    if($request.url.indexOf(`${version}?`) > -1){
-        $notify("TileRPG", "", "文件重定向");
-        var mStatus = "HTTP/1.1 302 Found";
-        var mHeaders = {"Location": $request.url.replace(version,"0")};
-        var mResponse = {
-            status:mStatus,
-            headers:mHeaders
-        }
+    //  var version = $prefs.valueForKey('tileRPG_version');
+    // if($request.url.indexOf(`${version}?`) > -1){
+    //     $notify("TileRPG", "", "文件重定向");
+    //     var mStatus = "HTTP/1.1 302 Found";
+    //     var mHeaders = {"Location": $request.url.replace(version,"0")};
+    //     var mResponse = {
+    //         status:mStatus,
+    //         headers:mHeaders
+    //     }
 
-        $done(mResponse);
-    }else{
-        $done({});
+    //     $done(mResponse);
+    // }else{
+    //     $done({});
+    // }
+    var versionRequest = {
+        url:'https://tilerpglive.mafrpgserver.net/v0/gameData/table/getGameTableUpperVersion/0?version=1.15.69&&flatform=ios&&table_version=0&&useridx=0&&loginToken=0&&country=jp&&server=1'
     }
+    $task.fetch(versionRequest).then(response=>{
+        $notify("TileRPG", "", "全配置加载完毕");
+        $done(response.body);
+    })
     
 }else{
     var body = JSON.parse($response.body);
